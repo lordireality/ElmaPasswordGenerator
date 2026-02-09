dcdab696e21658c3fb330a9ae561511f27a8c077
lordireality <lordireality@gmail.com>
2026-02-09 13:20:19 +0300

    Добавьте файлы проекта.

---
+++ ElmaPasswordGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ElmaPasswordGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
